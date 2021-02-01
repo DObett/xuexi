2ed36afe28fef92e7894199a8095cbbfeebb147e
王家琦 <1165849643@qq.com>
2021-02-01 09:22:55 +0800

    123

---
+++ Zuoye/src/test/java/org/example/StepDefinitions.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+    https://git.youlu.com/wanma/hr-notice-api-test.git
+
+}
