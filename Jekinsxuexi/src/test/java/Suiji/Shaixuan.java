942cacd9bd4ffb91c9687e8087698525bbabfee4
王家琦 <1165849643@qq.com>
2021-02-21 16:30:17 +0800

    'quchongpaixu'

--- Jekinsxuexi/src/test/java/Suiji/Shaixuan.java
+++ Jekinsxuexi/src/test/java/Suiji/Shaixuan.java
@@ -4,10 +4,33 @@ import java.util.*;
 import dao.rec;
 public class Shaixuan {
     public static void main(String[] args) throws SQLException {
-         rec recoder =new rec();
+        rec recoder = new rec();
+        Map<String, String> mapp = new HashMap<>();
+        mapp=recoder.selectRecord();
+        Set set = mapp.keySet();
+        System.out.println(mapp.size());
+        for(Iterator iter = set.iterator(); iter.hasNext();)
+        {
+            String key = (String)iter.next();
+            String value = (String)mapp.get(key);
+           System.out.println(key+"===="+value);
+           // System.out.println(value);
 
-        System.out.println(recoder.selectRecord());
+
+        }
+
+
+        HashMap map = new HashMap();
+
+        map.put("a","aaaa");
+        map.put("b","bbbb");
+        map.put("c","cccc");
+        map.put("d","dddd");
+        //System.out.println(map.size());
 
     }
 
-}
+
+
+    }
+
--- Jekinsxuexi/src/test/java/dao/rec.java
+++ Jekinsxuexi/src/test/java/dao/rec.java
@@ -1,7 +1,10 @@
 package dao;
 
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Date;
 
 public class rec {
     String baofu;
@@ -46,22 +49,25 @@ public class rec {
 
     }
 
-    public Map<String, List<String>> selectRecord() throws SQLException {
+    public Map<String, String> selectRecord() throws SQLException {
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
         rec record = new rec();
        // JSONObject object = new JSONObject();
         Map<String, List<String>> map = new HashMap<>();
+
         List<String> groupNameList = new ArrayList<String>();//
         List<String> orderid = new ArrayList<String>();
+        Map<String, String> mapp = new HashMap<>();
+
         try {
             //获得数据的连接
             conn = JDBCUtils.getConnection();
             //获得Statement对象
             stmt = conn.createStatement();
             //发送SQL语句
-            String sql = "SELECT * from `daili`.`0205`;";
+            String sql = "SELECT * from daili.0206;";
 
             // 执行更新，返回受影响行数
           //  rs = stmt.executeQuery(sql);
@@ -70,57 +76,137 @@ public class rec {
 
 
 
+
             int rowCount = 0;
 
 // 循环结果
+            List<String> orderids = new ArrayList<String>();
+            orderids.add("2020-11-12 00:00:00");
+            orderids.add("2019-01-21 11:35:03");
+            orderids.add("2019-02-22 18:00:55");
+            orderids.add("2019-01-12 18:00:55");
+            orderids.add("2019-05-22 18:00:55");
+            orderids.add("2019-06-22 18:00:55");
+            orderids.add("2019-01-12 18:00:55");
+
+
+
+             record.compareriqi(orderids);
+            System.out.println(record.compareriqi(orderids));
+
+
+
+            for (int x = 0; x <= rowCount; x++) {
+
                 while (rs.next()) {
-                 record.setId(rs.getString(1));
-               record.setSenten(rs.getString(2));
+                    record.setId(rs.getString(1));
+                    record.setSenten(rs.getString(2));
 
 
-              // for (int x=0;x<rowCount;x++){
 
 
-               if (rowCount==0){
-                   orderid.add(record.getId());
-                   groupNameList.add(record.getSenten());
-                   map.put(orderid.get(rowCount), groupNameList);
+                        if (rowCount == 0) {
+                            orderid.add(record.getId());
+                            groupNameList.add(record.getSenten());
+                            mapp.put(orderid.get(rowCount), record.listToString(groupNameList,groupNameList));
 
-            }else if (  orderid.get(rowCount-rowCount).equals(record.getId())) {
-                   groupNameList.add(record.getSenten());
-                  map.put(orderid.get(rowCount-rowCount), groupNameList);
-                   System.out.println(map);
-               } else {
-                   List<String>  String.value.of(orderid.size)  = new ArrayList<String>();
-                   orderid.clear();
-                   groupNameList.clear();
-                   orderid.add(record.getId());
-                   groupNameList.add(record.getSenten());
-                   map.put(record.getId(), groupNameList);
-               }
+                        } else if (orderid.get(rowCount - rowCount).equals(record.getId())) {
+                            groupNameList.add(record.getSenten());
+                            record.compareriqi(groupNameList);
+                           mapp.put(orderid.get(rowCount - rowCount),   record.compareriqi(groupNameList));
+                           // mapp.put(orderid.get(rowCount - rowCount),  record.compareriqi(groupNameList));
+                          //  record.listToString(groupNameList,groupNameList);
+
+                        } else {
+
+
+                            //  String[] strings = groupNameList.toArray(new String[groupNameList.size()]);
+                            // map.put(orderid.get(rowCount-rowCount), Arrays.asList(strings));
+
+
+                            //mapp.put(orderid.get(rowCount - rowCount), record.listToString(groupNameList,groupNameList));
+                            mapp.put(orderid.get(rowCount - rowCount),  record.compareriqi(groupNameList));
+                            orderid.clear();
+                            groupNameList.clear();
+                            orderid.add(record.getId());
+                            groupNameList.add(record.getSenten());
+
+//                            continue;
+
+
+
+                        }
+
                     rowCount++;
-           }
 
+                    }
 
+                }
+           // System.out.println(mapp.size());
+            System.out.println(rowCount);
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             JDBCUtils.release(rs, stmt, conn);
         }
-          return map;
+          return mapp;
+
  
     }
 
-    
-
-    
-
-
-    
 
 
 
+
+
+
+
+
+
+
+
+
+
+    public String listToString(List list ,List<String> separator) {
+        StringBuilder sb = new StringBuilder();
+ for (int i = 0; i < list.size(); i++) {
+     if (i == list.size() - 1) {
+         sb.append(list.get(i));
+     } else {
+         sb.append(list.get(i)+" ");
+//sb.append(separator);
+
+     }
+ }
+   return sb.toString();
+    }
+
+
+    public   String  compareriqi (List<String> separator) throws ParseException {
+        SimpleDateFormat sdf  =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            for (int x=0;x<=separator.size();x++){
+                for (int y=0;y<separator.size()-1;y++) {
+                    String date = separator.get(y);Date dateD = sdf.parse(date);
+
+                    String date1 = separator.get(y + 1);  Date datee = sdf.parse(date1);
+
+
+
+                    if (dateD.getTime() >= datee.getTime()) {
+
+
+                        separator.set(y,date1);
+                        separator.set(y+1,date);
+
+                    }
+                }
+
+            }
+
+
+        return separator.get(0)  ;
+    }
 
 
 
@@ -137,3 +223,5 @@ public class rec {
 
 
 }
+
+
